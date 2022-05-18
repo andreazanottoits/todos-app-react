c519e69d1fdd2503a1a6910ff2817d36fb7d3580
andreazanottoits <andrea.zanotto@stud.tecnicosuperiorekennedy.it>
2022-05-18 12:06:26 +0200

    Add delete todo func as prop

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,13 +1,19 @@
 import { Flex, Text, Button } from '@chakra-ui/react';
 
-export default function ToDoList({ toDoList }) {
+export default function ToDoList({ toDoList, deleteTodoFunc }) {
   return (
     <Flex flexDirection={'column'} height={'100vh'} width={'100vh'}>
       {toDoList.map((item, index) => (
         <Flex mt={'25'} key={index}>
           <Text flexGrow={'2'}>{item}</Text>
           <Button colorScheme="yellow">Modifica</Button>
-          <Button ml={'5'} colorScheme="red">Elimina</Button>
+          <Button
+            ml={'5'}
+            colorScheme="red"
+            onClick={() => deleteTodoFunc(index)}
+          >
+            Elimina
+          </Button>
         </Flex>
       ))}
     </Flex>
