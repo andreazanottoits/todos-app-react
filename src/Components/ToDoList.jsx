8889a76ba82b08c706def7f77146f165d7775819
andreazanottoits <andrea.zanotto@stud.tecnicosuperiorekennedy.it>
2022-05-18 15:34:19 +0200

    Add table visualization

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,23 +1,56 @@
 import { Flex, Text, Button } from '@chakra-ui/react';
 
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react';
+
 export default function ToDoList({ toDoList, deleteTodoFunc, modifyToDoFunc }) {
   return (
     <Flex flexDirection={'column'} height={'100vh'} width={'100vh'}>
-      {toDoList.map((item, index) => (
-        <Flex mt={'25'} key={index}>
-          <Text flexGrow={'2'}>{item}</Text>
-          <Button colorScheme="yellow" onClick={() => modifyToDoFunc(index)}>
-            Modifica
-          </Button>
-          <Button
-            ml={'5'}
-            colorScheme="red"
-            onClick={() => deleteTodoFunc(index)}
-          >
-            Elimina
-          </Button>
-        </Flex>
-      ))}
+      <TableContainer mt={'10'}>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Text</Th>
+              <Th>Timespant</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {toDoList.map((item, index) => (
+              <Tr key={index}>
+                <Td>
+                  <Text>{item.Text}</Text>
+                </Td>
+                <Td>
+                  <Text>{item.Timestamp}</Text>
+                </Td>
+                <Td>
+                  <Button
+                    colorScheme="yellow"
+                    onClick={() => modifyToDoFunc(index)}
+                  >
+                    Modifica
+                  </Button>
+                  <Button
+                    ml={'5'}
+                    colorScheme="red"
+                    onClick={() => deleteTodoFunc(index)}
+                  >
+                    Elimina
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Flex>
   );
 }
